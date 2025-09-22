01a7b59557c5de8ad695f20db838fb1f474e8e07
PritamDevs <debpritam2004@gmail.com>
2025-09-23 03:18:21 +0530

    v1.1.4

--- src/config/index.js
+++ src/config/index.js
@@ -1,23 +1,8 @@
 
-// export const BACKEND_URL="https://clms-backend-1gkb.onrender.com"
-
-// // export const BACKEND_URL="http://localhost:5505/api"
-
-// config/index.js
-// export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-
-// // Optional: fallback for safety
-// if (!BACKEND_URL) {
-//   console.warn(
-//     "VITE_BACKEND_URL is not defined. Defaulting to http://localhost:5505/api"
-//   );
-// }
-
-
 // Local development backend
-export const BACKEND_URL = "http://localhost:5505";
+// export const BACKEND_URL = "http://localhost:5505";
 
 // Production backend
-// export const BACKEND_URL = "https://clms-backend-1gkb.onrender.com/api";
+export const BACKEND_URL = "https://clms-backend-1gkb.onrender.com";
 
 console.log("Using backend URL:", BACKEND_URL);
